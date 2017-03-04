58cbca6e17a2d91e56d8e6266ba12dd443ebfc07
Mariusz Kozakowski <mkozakowski@squiz.pl>
2017-03-04 18:16:12 +0100

    Add log path

--- ansible_install.sh
+++ ansible_install.sh
@@ -3,4 +3,4 @@
 yum install -y epel-release
 yum install -y ansible git
 
-ansible-pull -U https://github.com/11mariom/azure-quickstart-magento.git
+ansible-pull -U https://github.com/11mariom/azure-quickstart-magento.git >> /var/log/ansible_pull.log 2>&1
